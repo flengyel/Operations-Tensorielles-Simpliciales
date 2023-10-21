f1104c959fcc8a9ba1d8ed5ed6d615a946f741e3
flengyel <florian.lengyel@gmail.com>
2023-10-21 18:59:16 -0400

    Type annotations.

--- tensor_rank_dim_conjecture.py
+++ tensor_rank_dim_conjecture.py
@@ -1,8 +1,9 @@
+from typing import Tuple # , List, Union, Any
 from operations_tensorielles_simpliciales import tensor_inner_horn_rank_dimension_conjecture
 from operations_tensorielles_simpliciales import tensor_inner_horn_rank_dimension_comparison
 import random
 
-def random_shape(n=14):
+def random_shape(n: int) -> Tuple[int]:
     length = random.randint(2, n/2)  # Length of at least two and bounded by 10
     return tuple(random.randint(2, n) for _ in range(length))  # Positive integers at least two and bounded by 10
 
@@ -10,7 +11,7 @@ if __name__ == "__main__":
     non_unique_horns = 0
     unique_horns = 0
     for i in range(1000):
-        shape = random_shape()
+        shape = random_shape(14)
         print(i ,": Shape = ", shape)
         comparison = tensor_inner_horn_rank_dimension_comparison(shape, verbose=True)
         conjecture = tensor_inner_horn_rank_dimension_conjecture(shape, verbose=True)
