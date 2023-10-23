7f2ba009ee92b80eb6917677d4185013ddf052ac
flengyel <florian.lengyel@gmail.com>
2023-10-23 00:21:51 -0400

    LICENSE, copyright

--- tensor_rank_dim_conjecture.py
+++ tensor_rank_dim_conjecture.py
@@ -1,3 +1,22 @@
+#    Opérations Tensorielles Simpliciales
+#    Simplicial Operations on Matrices and Hypermatrices
+#
+#    Copyright (C) 2021-2023 Florian Lengyel
+#    Email: florian.lengyel at cuny edu, florian.lengyel at gmail
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from typing import Tuple # , List, Union, Any
 from operations_tensorielles_simpliciales import tensor_inner_horn_rank_dimension_conjecture
 from operations_tensorielles_simpliciales import tensor_inner_horn_rank_dimension_comparison
